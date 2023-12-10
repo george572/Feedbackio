5c37eada32a4edeb909cee8dbc669194e458824a
george572 <xutiashviligiorgi@gmail.com>
2023-12-10 22:24:27 +0400

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  plugins: ["tailwindcss"],
+  extends: [
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "comma-dangle": ["error", "always-multiline"],
+    semi: ["error", "always"],
+    "func-call-spacing": "warn",
+    "no-case-declarations": "off",
+    "object-curly-spacing": ["error", "always"],
+    "vue/multi-word-component-names": "off",
+    "vue/no-reserved-component-names": "off",
+    "vue/no-multiple-template-root": "off",
+    "vue/no-template-shadow": "off",
+    "vue/v-on-event-hyphenation": ["error", "always"],
+    "tailwindcss/classnames-order": "off",
+    "tailwindcss/no-contradicting-classname": "warn",
+  },
+};
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h2 class="text-3xl text-green-400">
+      Compnents here
+    </h2> 
+  </main>
+</template>
