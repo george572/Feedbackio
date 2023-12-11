96900c5e5d15fc6bfda783c4e60019f648ddf507
george572 <xutiashviligiorgi@gmail.com>
2023-12-12 02:24:22 +0400

    renamed components + added app info component

---
+++ src/components/sidebar/Sidebar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SidebarAppInfo from './SidebarAppInfo.vue';
+</script>
+
+<template>
+  <div class="max-w-[255px] w-full flex flex-col gap-6">
+    <SidebarAppInfo />
+  </div>
+</template>
