5aa3db59eaec1273b23594482f67666a1c3e34c9
george572 <xutiashviligiorgi@gmail.com>
2023-12-12 02:46:48 +0400

    added filters component UI

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import SidebarAppInfo from './SidebarAppInfo.vue';
+import SidebarFilters from './sidebar-filters/SidebarFilters.vue';
 </script>
 
 <template>
   <div class="max-w-[255px] w-full flex flex-col gap-6">
     <SidebarAppInfo />
+    <SidebarFilters />
   </div>
 </template>
---
+++ src/components/sidebar/sidebar-filters/SidebarFilters.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import SidebarFiltersTag from './SidebarFiltersTag.vue';
+import { ref } from 'vue';
+
+const tags = ref([
+  { title: "All", value: 'all', isActive: false },
+  { title: "UI", value: 'ui', isActive: false },
+  { title: "UX", value: 'ux', isActive: true },
+  { title: "Enhancement", value: 'enhancement', isActive: false },
+  { title: "Bug", value: 'bug', isActive: false },
+  { title: "Feature", value: 'feature', isActive: false },
+]);
+
+const filterByTag = ( tagName: string) => {
+  console.log('Filter requested by tag: ', tagName);
+};
+</script>
+
+<template>
+  <div class="w-full bg-white rounded-xl h-[166px] px-6 py-4 flex flex-wrap gap-2 items-start gap-y-3.5">
+    <SidebarFiltersTag
+      v-for="tag in tags"
+      :key="tag.value"
+      :tag-name="tag.value"
+      :is-active="tag.isActive"
+      @filter-by-tag="(tagName: string) => filterByTag(tagName)"
+    >
+      <span class="capitalize">{{ tag.title }}</span>
+    </SidebarFiltersTag>
+  </div>
+</template>
