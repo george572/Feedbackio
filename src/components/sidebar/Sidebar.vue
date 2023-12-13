df30b547456f8304150476a2e72a852ee22301dd
george572 <xutiashviligiorgi@gmail.com>
2023-12-13 14:41:42 +0400

    added roadmap ui

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import SidebarAppInfo from './SidebarAppInfo.vue';
 import SidebarFilters from './sidebar-filters/SidebarFilters.vue';
+import SidebarRoadmap from './sidebar-roadmap/SidebarRoadmap.vue';
 </script>
 
 <template>
   <div class="max-w-[255px] w-full flex flex-col gap-6">
     <SidebarAppInfo />
     <SidebarFilters />
+    <SidebarRoadmap />
   </div>
 </template>
---
+++ src/components/sidebar/sidebar-roadmap/SidebarRoadmap.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import SidebarRoadmapItem from './SidebarRoadmapItem.vue';
+
+const tasksData = [
+  { type: 'planned', tasks: 12 },
+  { type: 'inProgress', tasks: 3 },
+  { type: 'live', tasks: 0 },
+];
+</script>
+
+<template>
+  <div class="w-full h-[178px] rounded-xl bg-white py-5 px-6">
+    <div class="flex items-center justify-between">
+      <h2 class="font-[Jost-Bold] text-lg text-darkBlue">
+        Roadmap
+      </h2>
+      <a
+        href="#!"
+        class="underline text-blue text-[13px] font-[Jost-Semibold]"
+      >View</a>
+    </div>
+    <div class="flex flex-col gap-2 mt-6">
+      <SidebarRoadmapItem
+        v-for="task in tasksData"
+        :key="task.type"
+        :task-type="task.type"
+        :tasks-amount="task.tasks"
+      />
+    </div>
+  </div>
+</template>
