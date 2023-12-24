0d8711e70ef6953fecf797a8cf08806acb5d1ab9
george572 <xutiashviligiorgi@gmail.com>
2023-12-25 00:38:14 +0400

    Added screen to handle empty feedback scenarios

--- src/components/suggestions-list/SuggestionsList.vue
+++ src/components/suggestions-list/SuggestionsList.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import SuggestionsHeader from './SuggestionsHeader.vue';
 import SuggestionItem from './SuggestionItem.vue';
+import SuggestionsListEmpty from './SuggestionsListEmpty.vue';
 import { onMounted, computed } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useSuggestionsStore } from '../../stores/suggestionsStore';
@@ -26,22 +27,36 @@ onMounted(() => {
 <template>
   <div class="w-full px-0 lg:px-0">
     <SuggestionsHeader :total-suggestions="totalSuggestions" />
-    <div class="relative px-6 mt-6 overflow-hidden md:px-0">
-      <transition-group
-        name="list"
-        tag="ul"
+    <Transition
+      name="fade"
+      mode="out-in"
+    >
+      <div
+        v-if="filteredSuggestionsData.length > 0"
+        class="relative px-6 mt-6 overflow-hidden md:px-0"
       >
-        <SuggestionItem
-          v-for="suggestion in filteredSuggestionsData"
-          :key="suggestion.id"
-          :upvotes-amount="suggestion.upvotes"
-          :title="suggestion.title"
-          :description="suggestion.description"
-          :tag="suggestion.category"
-          :comments-amount="suggestion.comments ? suggestion.comments.length : 0"
-        />
-      </transition-group>
-    </div>
+        <transition-group
+          name="list"
+          tag="ul"
+        >
+          <SuggestionItem
+            v-for="suggestion in filteredSuggestionsData"
+            :key="suggestion.id"
+            :upvotes-amount="suggestion.upvotes"
+            :title="suggestion.title"
+            :description="suggestion.description"
+            :tag="suggestion.category"
+            :comments-amount="suggestion.comments ? suggestion.comments.length : 0"
+          />
+        </transition-group>
+      </div>
+      <div
+        v-else
+        class="px-5 md:px-0"
+      >
+        <SuggestionsListEmpty />
+      </div>
+    </Transition>
   </div>
 </template>
 
@@ -60,5 +75,15 @@ onMounted(() => {
 
 .list-leave-active {
   position: absolute;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>../../stores/suggestionsStore
---
+++ src/components/suggestions-list/SuggestionsListEmpty.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import AppButton from '../base/AppButton.vue';
+</script>
+
+<template>
+  <div class="max-h-[600px] py-28 flex items-center justify-center flex-col bg-white rounded-xl my-6 px-5 w-full">
+    <img
+      src="../../assets/images/empty-icon.png"
+      alt=""
+      class="mb-10"
+    >
+    <div class="max-w-[410px]">
+      <h2 class="capitalize font-[Jost-Bold] text-2xl text-darkBlue text-center mb-5">
+        There is no feedback yet.
+      </h2>
+    </div>
+    <div class="max-w-[410px] mb-5">
+      <p class="capitalize font-[Jost-Regular] text-base text-center leading-[16px] text-gray">
+        Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.
+      </p>
+    </div>
+    <AppButton :bg-color="'purple'">
+      + Add Feedback
+    </AppButton>
+  </div>
+</template>
